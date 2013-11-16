2af4a31b024adba16f505fb03d4abce9768773a9
Jian Yu <askingyj@gmail.com>
2013-11-16 11:58:20 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import simplerun
+
+from setuptools import setup
+
+setup(
+    name='simplerun',
+    version=simplerun.__version__,
+    author='netspyer',
+    packages=['simplerun'],
+    license='GPL v2')
